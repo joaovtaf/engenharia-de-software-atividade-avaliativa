49334aa84b5a4f26c245403799c2fa3e3be8c7fc
João Vitor Ferreira <joaovtaf@gmail.com>
2026-09-22 11:02:39 -0300

    OrderService e EventLogger

---
+++ src/service.py
@@ -0,0 +1,21 @@
+# Questão 6
+
+class EventLogger:
+    def log(self, order):
+        print(f"[log] pedido de {order.cliente} processado, total {order.total()}")
+
+
+class OrderService:
+    def __init__(self, payment_processor, channel_factory, logger):
+        self.payment_processor = payment_processor
+        self.channel_factory = channel_factory
+        self.logger = logger
+
+    def process(self, order):
+        checkout = self.channel_factory.create_checkout()
+        notification = self.channel_factory.create_notification()
+
+        checkout.show(order)
+        self.payment_processor.process_order(order)
+        notification.send(order)
+        self.logger.log(order)

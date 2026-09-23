6569913495c9838aae4c24ee4e4413bb45202228
João Vitor Ferreira <joaovtaf@gmail.com>
2026-09-23 10:03:43 -0300

    gift card registrado no dicionario de pagamentos

--- src/giftcard.py
+++ src/giftcard.py
@@ -1,11 +1,16 @@
-# Questão 8
+# Questão 8, forma de pagamento nova sem mexer no payment.py
 
-from src.payment import Payment, PaymentProcessor
+from src.payment import Payment, PaymentProcessor, register_payment_processor
+
 
 class GiftCardPayment(Payment):
     def pay(self, amount) -> None:
-        print(f"Pagamento de {amount} em GiftCard realizado com sucesso.")
+        print(f"Pagamento de {amount} em gift card realizado com sucesso!")
+
 
 class GiftCardProcessor(PaymentProcessor):
     def create_payment(self) -> Payment:
         return GiftCardPayment()
+
+
+register_payment_processor("GIFTCARD", GiftCardProcessor)
